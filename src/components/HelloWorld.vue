b8a2908cb9dc9d36f93433a6f7e3e9334a47acd6
Pierre Beurtheret <pierrebeur0@gmail.com>
2025-06-15 15:54:55 -0700

    Scaffold Vue 3 + Vite + CRXJS

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
+defineProps({
+  msg: String,
+})
 
 const count = ref(0)
 </script>
@@ -10,28 +12,23 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count++">
+      count is {{ count }}
+    </button>
     <p>
       Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      <code>src/components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://github.com/crxjs/create-crxjs" target="_blank">create-crxjs</a>, the official starter
   </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
+
+  <p class="read-the-docs">
+    Click on the Vite, Vue and CRXJS logos to learn more
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from 'node:path'
 import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json' assert { type: 'json' }
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import zip from 'vite-plugin-zip-pack'
+import manifest from './manifest.config.ts'
+import { name, version } from './package.json'
 
-// https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': `${path.resolve(__dirname, 'src')}`,
+    },
+  },
   plugins: [
     vue(),
     crx({ manifest }),
+    zip({ outDir: 'release', outFileName: `crx-${name}-${version}.zip` }),
   ],
+  server: {
+    cors: {
+      origin: [
+        /chrome-extension:\/\//,
+      ],
+    },
+  },
 })
